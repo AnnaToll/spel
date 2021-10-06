4692ace25258f2681d573c350e1bb77ad5ffe796
Anna Toll <anna.toll@protonmail.com>
2021-10-06 22:56:08 +0200

    Spelet fungerar  :)

---
+++ uppgift5.js
@@ -0,0 +1,153 @@
+/*
+Med den kunskap ni fått från föregående 9 uppgifter, skapa spelet sten/sax/påse som kan spelas mot datorn. 
+Spela det verkliga spelet med en vän för att se hur processen går till. 
+Översätt sedan detta till pseudokod eller en flowchart innan ni börjar programmera. 
+Kom ihåg att bryta ner problemet i flera moduler och få dessa att funka separat.
+
+Parprogrammering uppmuntras. Använd er av tekniker från tidigare uppgifter för att lösa denna uppgift.
+*/
+
+/* let gameChoices = ['sten', 'sax', 'påse'];
+
+let randomChoice = gameChoices[2];
+
+console.log(randomChoice);
+console.log(gameChoices.length);
+
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];*/
+
+function spel() {
+    let counter = 0;
+    
+    let pointsComputer = 0,
+    pointsPlayer = 0;
+    
+    var nummer = Math.random();
+    console.log(nummer);
+    
+    var nummer2 = Math.random();
+    console.log(nummer2);
+    
+    var nummer3 = Math.random();
+    console.log(nummer3);
+    
+    do {
+    
+        counter++;
+    
+        if (counter == 1) {
+            nummer = nummer;
+        }
+    
+        else if (counter == 2) {
+            nummer = nummer2;
+        }
+    
+        else {
+            nummer = nummer3;
+        }
+    
+        if (nummer >= 0 && nummer < 0.333) {
+            nummer = 'sten';
+        }
+        
+        else if (nummer >= 0.333 && nummer < 0.666) {
+            nummer = 'sax';
+        }
+        
+        else {
+            nummer = 'påse';
+        }
+        
+        console.log(nummer);
+    
+        let usersChoice;
+
+        do {
+
+            if (counter == 1) {
+                usersChoice = prompt(`Vågar du anta utmaningen? Sten, sax eller påse mot datorn, där bäst av tre vinner :) Välj sten, sax eller påse.`).toLowerCase();
+                // if ((usersChoice == null) || (usersChoice == ''))  {
+                //     return;
+                // }
+        
+            }
+            
+            else if (counter == 2) {
+                usersChoice = prompt(`Omgång 2, Ding ding!! Välj sten, sax eller påse.
+                
+            
+        Poängställning: ${pointsComputer} (dator) / ${pointsPlayer} (du).`).toLowerCase();
+                // if ((usersChoice == null) || (usersChoice == '')) {
+                //     return;
+                // }
+            }
+            
+            else if (counter == 3) {
+                usersChoice = prompt(`Final Round! Spänningen är olidlig!! Välj sten, sax eller påse.
+                
+        Poängställning: ${pointsComputer} (dator) / ${pointsPlayer} (du).`).toLowerCase();
+                // if ((usersChoice == null) || (usersChoice == '')) {
+                //     return;
+                //  }
+            }
+    
+            // usersChoice = usersChoice.toLowerCase();
+            
+            if (nummer == usersChoice) {
+                    pointsComputer++;
+                    pointsPlayer++;
+                    // if (counter < 3) {
+                        alert(`Oavgjort, båda valde ${usersChoice}! Ni får ett poäng var :)`);
+                    // }
+            }
+            
+            else if ((nummer == 'sten' && usersChoice == 'sax') || (nummer == 'sax' && usersChoice == 'påse') || (nummer == 'påse' && usersChoice == 'sten')) {
+                pointsComputer++;
+                // if (counter < 3) {
+                    alert(`Åh nej!! Datorn valde ${nummer} och du valde ${usersChoice} :(( En poäng till datorn.`);
+                // }
+            } 
+            
+            else if ((nummer == 'sten' && usersChoice == 'påse') || (nummer == 'sax' && usersChoice == 'sten') || (nummer == 'påse' &&  usersChoice == 'sax')) {
+                pointsPlayer++;
+                // if (counter < 3) {
+                    alert(`Bra jobbat!! Datorn valde ${nummer} och du valde ${usersChoice} :)`);
+                // }
+            }
+    
+            else {
+                if ((usersChoice == null) || (usersChoice == '')) {
+                    return;
+                 }
+                 else {
+                     alert(`Du kan inte spela spelet med ${usersChoice} tokboll. Endast sten, sax, eller påse är tillåtet. Försök gärna igen :)`);
+                     
+                 }
+        
+            }
+        
+
+        } while (usersChoice != 'sten' && usersChoice != 'sax' && usersChoice != 'påse');
+
+    console.log('Omgång' + counter);
+    console.log('your points' + pointsPlayer);
+    console.log('computer points' + pointsComputer);
+        
+    } while (counter < 3);
+    
+    if (pointsPlayer == pointsComputer) {
+        alert(`Matchen slutade oavgjort!! Du fick ${pointsPlayer} poäng och datorn fick ${pointsComputer} poäng :) Spela en gång till vetja :D `)
+    }
+    
+    else if (pointsPlayer > pointsComputer) {
+        alert(`Whooopwhoooop, grattis du vann matchen!! Det blev ${pointsPlayer} / ${pointsComputer} till dig :) Kan du klå datorn igen? Spela en gång till vetja :D`);
+    }
+    
+    else if (pointsPlayer < pointsComputer) {
+        alert(`Åh nej!! Det blev ${pointsComputer} / ${pointsPlayer} till datorn :( Sugen på revansch? Spela en gång till vetja :D`);
+    }
+
+}
+
+
